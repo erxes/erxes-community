03203c218dc447f9cda3ce64b9fa1d8a95917521
Tuguldur Ch <tuguldur.ch@nmma.co>
2022-09-15 18:01:08 +0800

    removed some unnessary codes

--- packages/plugin-forms-api/src/graphql/resolvers/mutations/forms.ts
+++ packages/plugin-forms-api/src/graphql/resolvers/mutations/forms.ts
@@ -51,7 +51,7 @@ const formMutations = {
       const formSubmission = await models.FormSubmissions.findOne({
         contentTypeId,
         contentType,
-        formFieldId,
+        formFieldId
       });
 
       if (formSubmission) {
