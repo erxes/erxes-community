879886a4c1507d13ee42dea686055e36588cc0e2
Wlkr123 <104069564+Wlkr123@users.noreply.github.com>
2022-09-21 16:28:14 +0800

    removed unnessary update

--- packages/plugin-riskassessment-api/src/utils.ts
+++ packages/plugin-riskassessment-api/src/utils.ts
@@ -55,8 +55,19 @@ export const calculateRiskAssessment = async (models, subdomain, cardId, formId)
 
   for (const submission of submissions) {
     const { optionsValues } = fields.find(field => field._id === submission.fieldId);
-    const fieldValue = optionsValues.find(option => option.label === submission.value);
-
+    const optValues = optionsValues
+      .split('\n')
+      .map(item => {
+        if (item.match(/=/g)) {
+          const label = item?.substring(0, item.indexOf('='));
+          const value = parseInt(item.substring(item?.indexOf('=') + 1, item.length));
+          if (!Number.isNaN(value)) {
+            return { label, value };
+          }
+        }
+      }, [])
+      .filter(item => item);
+    const fieldValue = optValues.find(option => option.label === submission.value);
     switch (calculateMethod) {
       case 'Multiply':
         sumNumber *= parseInt(fieldValue.value);
@@ -65,7 +76,6 @@ export const calculateRiskAssessment = async (models, subdomain, cardId, formId)
         sumNumber += parseInt(fieldValue.value);
         break;
     }
-    // sumNumber += parseInt(fieldValue.value);
   }
 
   for (const { name, value, value2, logic, color } of calculateLogics) {
@@ -76,11 +86,11 @@ export const calculateRiskAssessment = async (models, subdomain, cardId, formId)
           { _id: riskAssessmentId },
           { $set: { status: name, statusColor: color } },
           { new: true }
-          );
-        }
+        );
       }
+    }
     if (['>', '<'].includes(operator)) {
-      operator += '='
+      operator += '=';
       if (eval(sumNumber + operator + value)) {
         await models.RiskAssessment.findOneAndUpdate(
           { _id: riskAssessmentId },
