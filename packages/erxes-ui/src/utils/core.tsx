aa825cdf5af94f8a7dc721e0e61da3e05ccd9f1b
soyombo <e11iot.soko@gmail.com>
2022-10-11 13:03:36 +0800

    Update core.tsx

--- packages/erxes-ui/src/utils/core.tsx
+++ packages/erxes-ui/src/utils/core.tsx
@@ -39,15 +39,9 @@ export const loadDynamicComponent = (
   multi?: boolean,
   pluginName?: string
 ): any => {
-  console.log('componentName', componentName);
-
   const plugins: any[] = (window as any).plugins || [];
 
-  // console.log('plugins', plugins);
-
   const filteredPlugins = plugins.filter(plugin => plugin[componentName]);
-
-  // console.log('filteredPlugins', filteredPlugins);
 
   const renderDynamicComp = (plugin: any) => (
     <ErrorBoundary key={plugin.scope}>
@@ -72,12 +66,8 @@ export const loadDynamicComponent = (
       plugin => plugin.name === pluginName
     );
 
-    console.log('withPluginName', withPluginName);
-
     return renderDynamicComp(withPluginName[0]);
   }
-
-  console.log('filteredPlugins[0]', filteredPlugins[0]);
 
   return renderDynamicComp(filteredPlugins[0]);
 };
