9798168326f85ccf161587144d1e18e87748f9e5
Tuguldur Ch <tuguldur.ch@nmma.co>
2022-11-21 11:20:39 +0800

    update risk assessment

--- packages/plugin-riskassessment-api/src/dataLoaders/index.ts
+++ packages/plugin-riskassessment-api/src/dataLoaders/index.ts
@@ -4,20 +4,17 @@ import { IModels } from '../connectionResolver';
 import categories from './category';
 import riskAssessment from './riskAssessment';
 import user from './user';
-import formField from './formField';
 
 export interface IDataLoaders {
   riskAssessment: DataLoader<string, any>;
   categories: DataLoader<string, any>;
   user: DataLoader<string, any>;
-  formField: DataLoader<string, any>;
 }
 
 export function generateAllDataLoaders(models: IModels, subdomain: string): IDataLoaders {
   return {
     riskAssessment: riskAssessment(models),
     categories: categories(models),
-    user: user(models, subdomain),
-    formField: formField(models, subdomain)
+    user: user(models, subdomain)
   };
 }
