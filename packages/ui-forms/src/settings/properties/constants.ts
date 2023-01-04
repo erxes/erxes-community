a650ab0db239a6e079eac7c5fea5f901fa96528b
soyombo <e11iot.soko@gmail.com>
2023-01-04 16:49:52 +0800

    Update constants.ts

--- packages/ui-forms/src/settings/properties/constants.ts
+++ packages/ui-forms/src/settings/properties/constants.ts
@@ -5,14 +5,12 @@ export const FIELDS_GROUPS_CONTENT_TYPES = {
   CONVERSATION: 'inbox:conversation',
   DEVICE: 'contacts:device',
   USER: 'core:user',
-  CAR: 'cars:car',
   ALL: [
     'contacts:customer',
     'contacts:company',
     'products:product',
     'inbox:conversation',
     'contacts:device',
-    'core:user',
-    'cars:car'
+    'core:user'
   ]
 };
