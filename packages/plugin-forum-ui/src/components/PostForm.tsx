6ee33d1e395ee0a1fecf3b1250be92dfe869f7fa
Dulguun Otgon <dulguun0225@gmail.com>
2023-01-12 16:31:28 +0800

    admin ui prog

--- packages/plugin-forum-ui/src/components/PostForm.tsx
+++ packages/plugin-forum-ui/src/components/PostForm.tsx
@@ -1,18 +1,7 @@
 import React, { useState, useRef } from 'react';
 import PostContentEditor from './PostContentEditor';
 import CategorySelect from '../containers/CategorySelect';
-import { useQuery, useMutation } from 'react-apollo';
-import gql from 'graphql-tag';
-
-const QUERY_TAGS = gql`
-  query Tags {
-    tags(type: "forum:post") {
-      _id
-      colorCode
-      name
-    }
-  }
-`;
+import PollOptions from './PollOptions';
 
 const PostForm: React.FC<{ post?: any; onSubmit?: (any) => any }> = ({
   post,
@@ -25,12 +14,17 @@ const PostForm: React.FC<{ post?: any; onSubmit?: (any) => any }> = ({
   const [categoryId, setCategoryId] = useState(post?.categoryId || '');
   const [description, setDescription] = useState(post?.description || '');
 
-  const initialCheckedTagIds = {};
-  post?.tagIds?.forEach(id => {
-    initialCheckedTagIds[id] = true;
+  const initialPollOptionsById = {};
+  post?.pollOptions?.forEach(option => {
+    initialPollOptionsById[option._id] = option;
   });
 
-  const [checkedTagIds, setCheckedTagIds] = useState(initialCheckedTagIds);
+  const [pollOptionsById, setPollOptionsById] = useState(
+    initialPollOptionsById
+  );
+
+  const pollOptions: any[] = Object.values(pollOptionsById);
+  pollOptions.sort((a: any, b: any) => a.order - b.order);
 
   const editorRef = useRef<any>(null);
   const preSubmit = e => {
@@ -86,36 +80,42 @@ const PostForm: React.FC<{ post?: any; onSubmit?: (any) => any }> = ({
 
         <br />
 
-        <div>
-          <h5>Tags</h5>
-          <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-            {tagsQuery.data?.tags?.map(tag => (
-              <div style={{ margin: 5 }} key={tag._id}>
-                <input
-                  checked={!!checkedTagIds[tag._id]}
-                  onChange={e => {
-                    const checked = e.target.checked;
-                    setCheckedTagIds(prev => {
-                      const next = { ...prev };
-                      next[tag._id] = checked;
-                      return next;
-                    });
-                  }}
-                  type="checkbox"
-                  id={`tcb-${tag._id}`}
-                />{' '}
-                <label
-                  style={{ userSelect: 'none' }}
-                  htmlFor={`tcb-${tag._id}`}
-                >
-                  {tag.name}
-                </label>
-              </div>
-            ))}
-          </div>
+        <div style={{ margin: 20, padding: 20 }}>
+          Poll options:
+          <PollOptions
+            options={pollOptions}
+            onChange={(_id, title, order) => {
+              setPollOptionsById(prev => ({
+                ...prev,
+                [_id]: {
+                  _id,
+                  title,
+                  order
+                }
+              }));
+            }}
+            onAdd={() => {
+              const newPollOption = {
+                _id: Math.random()
+                  .toString(36)
+                  .slice(2),
+                title: '',
+                order: pollOptions[pollOptions.length - 1]?.order + 1 || 0,
+                isNew: true
+              };
+              setPollOptionsById(prev => ({
+                ...prev,
+                [newPollOption._id]: newPollOption
+              }));
+            }}
+            onRemove={_id => {
+              setPollOptionsById((prev: any) => {
+                const { [_id]: _, ...rest } = prev;
+                return rest;
+              });
+            }}
+          />
         </div>
-
-        <br />
 
         <button>Submit</button>
       </form>
