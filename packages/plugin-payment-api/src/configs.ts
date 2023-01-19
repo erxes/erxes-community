e19ff6cdf85c769fbf9298b493052ec5d08a8275
soyombo <e11iot.soko@gmail.com>
2023-01-19 19:42:23 +0800

    up

--- packages/plugin-payment-api/src/configs.ts
+++ packages/plugin-payment-api/src/configs.ts
@@ -9,6 +9,7 @@ import resolvers from './graphql/resolvers';
 import typeDefs from './graphql/typeDefs';
 import { initBroker } from './messageBroker';
 import { getHandler, postHandler } from './utils';
+import i18n = require('i18n');
 
 export let mainDb;
 export let debug;
@@ -80,6 +81,25 @@ export default {
 
     // generated scripts
     app.use('/build', express.static(path.join(__dirname, '../static')));
+
+    i18n.configure({
+      locales: ['en', 'mn'],
+      queryParameter: 'lang',
+      directory: __dirname + '/locales',
+      defaultLocale: 'en'
+    });
+
+    app.use(i18n.init);
+
+    app.use((req, _res, next) => {
+      const locale = req.query.lang || 'en';
+
+      console.log('locale', locale);
+
+      i18n.setLocale(locale);
+
+      next();
+    });
 
     app.use(controllers);
   }
