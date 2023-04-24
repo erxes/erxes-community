54f218af63a58494a9ce1619e007866b79ae730f
KhBaterdene <baterdne.k@gmail.com>
2023-04-24 17:56:06 +0800

    Merge branch 'dev' of github.com:erxes/erxes into order-improvement

--- packages/plugin-cards-api/src/messageBroker.ts
+++ packages/plugin-cards-api/src/messageBroker.ts
@@ -74,10 +74,20 @@ export const initBroker = async cl => {
 
   consumeRPCQueue('cards:deals.create', async ({ subdomain, data }) => {
     const models = await generateModels(subdomain);
+    const deals = await models.Deals.create(data);
 
+    const { customerId = '' } = data;
+
+    if (customerId) {
+      await createConformity(subdomain, {
+        customerIds: [customerId],
+        mainType: 'deal',
+        mainTypeId: deals._id
+      });
+    }
     return {
       status: 'success',
-      data: await models.Deals.create(data)
+      data: deals
     };
   });
 
@@ -96,9 +106,20 @@ export const initBroker = async cl => {
   consumeRPCQueue('cards:tickets.find', async ({ subdomain, data }) => {
     const models = await generateModels(subdomain);
 
+    if (!data.query) {
+      return {
+        status: 'success',
+        data: await models.Tickets.find(data).lean()
+      };
+    }
+
+    const { query, sort = {} } = data;
+
     return {
       status: 'success',
-      data: await models.Tickets.find(data).lean()
+      data: await models.Tickets.find(query)
+        .sort(sort)
+        .lean()
     };
   });
 
