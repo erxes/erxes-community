87a234f4ecf1489a4ed91906d9c86fd1005b7681
Bold Narantuya <epicbold@gmail.com>
2023-05-11 12:13:11 +0800

    edit function fix

--- packages/plugin-rcfa-ui/src/rcfa/components/Section.tsx
+++ packages/plugin-rcfa-ui/src/rcfa/components/Section.tsx
@@ -10,8 +10,12 @@ import {
   ControlLabel,
   __
 } from '@erxes/ui/src/';
-import { IRCFAQuestions } from '../../../../plugin-rcfa-api/src/models/definitions/rcfa';
+import { IRCFAQuestions as IRFCA } from '../../../../plugin-rcfa-api/src/models/definitions/rcfa';
 import { StyledContent, StyledQuestionItem } from '../../styles';
+
+interface IRCFAQuestions extends IRFCA {
+  editing: boolean;
+}
 
 type Props = {
   questions: IRCFAQuestions[];
@@ -25,17 +29,22 @@ type State = {
 };
 
 class Section extends React.Component<Props, State> {
-  state: { questions: IRCFAQuestions[] };
+  state: { questions: IRCFAQuestions[]; changeQuestion: '' };
 
   constructor(props) {
     super(props);
     this.state = {
-      questions: []
+      questions: [],
+      changeQuestion: ''
     };
   }
 
   componentWillReceiveProps(prevProps) {
-    this.setState({ questions: prevProps.questions });
+    const questions: any = [];
+    for (const question of prevProps.questions) {
+      questions.push({ editing: false, ...question });
+    }
+    this.setState({ questions: questions });
   }
 
   saveQuestion = () => {
@@ -56,10 +65,15 @@ class Section extends React.Component<Props, State> {
     this.setState({ questions: questList });
   };
 
-  editQuestion = (_id: string | undefined) => () => {
-    if (_id) {
-      this.props.editQuestion(_id, 'hello world');
-    }
+  editQuestion = (_id: string | undefined, index: number) => () => {
+    let questList = this.state.questions;
+    questList[index].editing = !questList[index].editing;
+    this.setState({ questions: questList });
+  };
+
+  saveEditedQuestion = (index: number) => () => {
+    let question = this.state.questions[index];
+    this.props.editQuestion(question._id as string, question.title);
   };
 
   deleteQuestion = (_id: string | undefined) => () => {
@@ -81,6 +95,19 @@ class Section extends React.Component<Props, State> {
     );
   };
 
+  onChangeQuestion = (index: number) => (event: any) => {
+    let questList = this.state.questions;
+    questList[index].title = event.target.value;
+    this.setState({ questions: questList });
+  };
+
+  cancelEdit = (index: number) => () => {
+    let questList = this.state.questions;
+    questList[index].title = this.props.questions[index].title;
+    questList[index].editing = false;
+    this.setState({ questions: questList });
+  };
+
   renderForm() {
     const trigger = (
       <Button btnStyle="simple">
@@ -88,11 +115,14 @@ class Section extends React.Component<Props, State> {
       </Button>
     );
 
-    const deleteModalTrigger = (_id: string | undefined, title: string) => (
-      <Button btnStyle="danger" size="small">
-        <Icon icon="times-circle" /> Delete
-      </Button>
-    );
+    const deleteModalTrigger = (_id: string | undefined, title: string) => {
+      console.log('aa');
+      return (
+        <Button btnStyle="danger" size="small">
+          <Icon icon="times-circle" /> Delete
+        </Button>
+      );
+    };
 
     const content = () => {
       return (
@@ -102,17 +132,51 @@ class Section extends React.Component<Props, State> {
               <p>
                 {__('Question')} {index + 1}:
               </p>
-              <p>{question.title}</p>
-
-              <Button
-                btnStyle="simple"
-                size="small"
-                onClick={this.editQuestion(question._id)}
-              >
-                <Icon icon="edit-alt" /> Edit
-              </Button>
-
-              {deleteModalTrigger(question._id, question.title)}
+              {question.editing ? (
+                <div style={{ marginBottom: '0.5rem' }}>
+                  <FormControl
+                    type="text"
+                    value={question.title}
+                    onChange={this.onChangeQuestion(index)}
+                  />
+                </div>
+              ) : (
+                <p
+                  style={{ marginTop: '1.0625rem', marginBottom: '0.9375rem' }}
+                >
+                  {question.title}
+                </p>
+              )}
+              <div style={{ textAlign: 'right' }}>
+                {question.editing ? (
+                  <div>
+                    <Button
+                      btnStyle="simple"
+                      size="small"
+                      onClick={this.cancelEdit(index)}
+                    >
+                      <Icon icon="times-circle" /> Cancel
+                    </Button>
+                    <Button
+                      size="small"
+                      onClick={this.saveEditedQuestion(index)}
+                    >
+                      <Icon icon="times-circle" /> Save
+                    </Button>
+                  </div>
+                ) : (
+                  <div>
+                    <Button
+                      btnStyle="simple"
+                      size="small"
+                      onClick={this.editQuestion(question._id, index)}
+                    >
+                      <Icon icon="edit-alt" /> Edit
+                    </Button>
+                    {deleteModalTrigger(question._id, question.title)}
+                  </div>
+                )}
+              </div>
             </StyledQuestionItem>
           ))}
 
@@ -136,6 +200,7 @@ class Section extends React.Component<Props, State> {
       title="delete question"
       trigger={deleteModalTrigger}
       content={this.deleteModalContent}
+      isOpen
     />;
 
     return <ModalTrigger title="RCFA" trigger={trigger} content={content} />;
