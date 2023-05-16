a1398ddec4e683bf11e926deb79a40c6ac39b7e2
Bold Narantuya <epicbold@gmail.com>
2023-05-16 10:59:19 +0800

    update

--- packages/plugin-rcfa-ui/src/rcfa/components/CreateTaskModal.tsx
+++ packages/plugin-rcfa-ui/src/rcfa/components/CreateTaskModal.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-// import BoardSelect from '@erxes/ui-cards/src/boards/containers/BoardSelect';
-import BoardSelect from '@erxes/ui-cards/src/boards/components/BoardSelect';
+import BoardSelect from '@erxes/ui-cards/src/boards/containers/BoardSelect';
+import { ControlLabel, FormGroup } from '@erxes/ui/src/components/form';
+import Select from 'react-select-plus';
+import TicketConvertTrigger from '@erxes/ui-cards/src/tickets/components/TicketConvertTrigger';
+import DealConvertTrigger from '@erxes/ui-cards/src/deals/components/DealConvertTrigger';
+import TaskConvertTrigger from '@erxes/ui-cards/src/tasks/components/TaskConvertTrigger';
+
+// sample
+// import ConvertTo from '../../../../plugin-inbox-ui/src/inbox/components/conversationDetail/workarea/ConvertTo';
 
 type Props = {};
 
@@ -9,9 +16,9 @@ type State = {
   stageId: string;
   pipelineId: string;
   boardId: string;
-  onChangeStage?: (stageId: string) => void;
-  onChangePipeline: (pipelineId: string, stages: any) => void;
-  onChangeBoard: (boardId: string) => void;
+  onChangeStage?: ((stageId: string) => void) | undefined;
+  onChangePipeline: ((pipelineId: string, stages: any) => void) | undefined;
+  onChangeBoard: ((boardId: string) => void) | undefined;
 };
 
 class CreateTaskModal extends React.Component<Props, State> {
@@ -28,27 +35,85 @@ class CreateTaskModal extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
 
-    // this.state = {
-    //   type: 'ticket',
-    //   stageId: '1',
-    //   pipelineId: '1',
-    //   boardId: '1'
-    // };
+    this.state = {
+      type: 'task',
+      stageId: '',
+      pipelineId: '',
+      boardId: '',
+      onChangeBoard(boardId: string): void {},
+      onChangePipeline(pipelineId: string, stages: any): void {},
+      onChangeStage(stageId: string): void {}
+    };
   }
 
   render() {
+    const options = [
+      { value: 'task', label: 'Task' },
+      { value: 'deal', label: 'Deal' }
+    ];
+
+    const onChange = (event: any) => {
+      this.setState({ type: event.value });
+    };
+
+    const onChangeBoard = (boardId: string) => {
+      console.log('onChangeBoard', boardId);
+      this.setState({ boardId: boardId });
+    };
+
+    const onChangePipeline = (value: any) => {
+      console.log('onChangePipeline', value);
+      this.setState({ pipelineId: value });
+    };
+
+    const onChangeStage = (stageId: string) => {
+      console.log('onChangeStage', stageId);
+      this.setState({ stageId: stageId });
+    };
+
+    const refetch = (): void => {};
+
+    const triggerProps: any = {
+      assignedUserIds: 'id',
+      relTypeIds: 'customerIds',
+      relType: 'customer',
+      sourceConversationId: 'id',
+      subject: '',
+      refetch: refetch
+    };
+
+    const url = 'url';
+
     return (
       <>
-        <p>Task modal</p>
+        <FormGroup>
+          <ControlLabel>Type</ControlLabel>
+          <Select
+            isRequired={true}
+            placeholder="Choose a type"
+            value={this.state.type}
+            onChange={onChange}
+            options={options}
+            clearable={false}
+          />
+        </FormGroup>
+
         <BoardSelect
-          type="ticket"
-          stageId="1"
-          pipelineId="1"
-          boardId="1"
-          onChangeStage="1"
-          onChangePipeline="1"
-          onChangeBoard="1"
+          type={this.state.type}
+          stageId={this.state.stageId}
+          pipelineId={this.state.pipelineId}
+          boardId={this.state.boardId}
+          onChangeStage={onChangeStage}
+          onChangePipeline={onChangePipeline}
+          onChangeBoard={onChangeBoard}
         />
+
+        {this.state.type === 'task' ? (
+          <TaskConvertTrigger {...triggerProps} />
+        ) : (
+          <DealConvertTrigger {...triggerProps} bookingProductId={url} />
+        )}
+        {/* <TicketConvertTrigger relType="" refetch={refetch} /> */}
       </>
     );
   }
