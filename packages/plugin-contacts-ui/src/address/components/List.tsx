b1d9b5baa001c9bfed796985fc3643f05cf2e865
soyombo <e11iot.soko@gmail.com>
2023-06-05 14:17:15 +0800

    Update List.tsx

--- packages/plugin-contacts-ui/src/address/components/List.tsx
+++ packages/plugin-contacts-ui/src/address/components/List.tsx
@@ -4,6 +4,12 @@ import React, { useState, useCallback } from 'react';
 
 import Row from './Row';
 import { IAddress } from '@erxes/ui-contacts/src/customers/types';
+import { FieldStyle, SidebarList } from '@erxes/ui/src/layout/styles';
+import { SidebarListItem } from '@erxes/ui-settings/src/styles';
+import ActionButtons from '@erxes/ui/src/components/ActionButtons';
+import Tip from '@erxes/ui/src/components/Tip';
+import Button from '@erxes/ui/src/components/Button';
+import LeftSidebar from '@erxes/ui/src/layout/components/Sidebar';
 
 type Props = {
   addresses: IAddress[];
@@ -23,31 +29,59 @@ const List = (props: Props) => {
     setAddresses(updatedAddresses);
   };
 
+  const onRemove = () => {
+    console.log('remove');
+  };
+
   const renderRow = () => {
-    return addresses.map((address, index) => (
-      <Row
-        key={address.osmId || Math.random().toString()}
-        address={{
-          ...address,
-          isEditing: address.osmId === props.currentAddress?.osmId
-        }}
-        index={index}
-        onChangeCheck={onChangeStatus}
-        onSelect={props.onSelect}
-      />
+    return (addresses || []).map((address, index) => (
+      <SidebarListItem
+        key={address.osmId}
+        isActive={address.osmId === props.currentAddress?.osmId}
+      >
+        <FieldStyle>
+          {address.short}
+          <p>{address.short}</p>
+        </FieldStyle>
+
+        <ActionButtons>
+          <Tip text={__('Delete')} placement="bottom">
+            <Button btnStyle="link" onClick={onRemove} icon="cancel-1" />
+          </Tip>
+        </ActionButtons>
+      </SidebarListItem>
     ));
+
+    // return addresses.map((address, index) => (
+    //   <Row
+    //     key={address.osmId || Math.random().toString()}
+    //     address={{
+    //       ...address,
+    //       isEditing: address.osmId === props.currentAddress?.osmId
+    //     }}
+    //     index={index}
+    //     onChangeCheck={onChangeStatus}
+    //     onSelect={props.onSelect}
+    //   />
+    // ));
   };
 
   return (
-    <Table whiteSpace="nowrap" hover={true}>
-      <thead>
-        <tr>
-          <th>{__('Addresses')}</th>
-          <th>{__('Default')}</th>
-        </tr>
-      </thead>
-      <tbody>{renderRow()}</tbody>
-    </Table>
+    <LeftSidebar wide={true} hasBorder={true}>
+      <SidebarList noTextColor={true} noBackground={true} id={'test'}>
+        {renderRow()}
+      </SidebarList>
+    </LeftSidebar>
+
+    // <Table whiteSpace="nowrap" hover={true}>
+    //   <thead>
+    //     <tr>
+    //       <th>{__('Addresses')}</th>
+    //       <th>{__('Default')}</th>
+    //     </tr>
+    //   </thead>
+    //   <tbody>{renderRow()}</tbody>
+    // </Table>
   );
 };
 
