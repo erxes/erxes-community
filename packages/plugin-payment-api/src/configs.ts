142c7916b8f02c9e7207bc5b46255dbbd307a1c5
soyombo <e11iot.soko@gmail.com>
2023-06-24 11:39:08 +0800

    Update configs.ts

--- packages/plugin-payment-api/src/configs.ts
+++ packages/plugin-payment-api/src/configs.ts
@@ -75,7 +75,7 @@ export default {
 
     const { app } = options;
 
-    app.set('views', path.join(__dirname + '/views'));
+    app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'pug');
 
     // serve static files
