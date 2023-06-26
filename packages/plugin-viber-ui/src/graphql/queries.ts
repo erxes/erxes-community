e935849361b1e4fcfa854e2eee88424cf500dbae
Bold Narantuya <epicbold@gmail.com>
2023-06-26 14:22:35 +0800

    code refactor

--- packages/plugin-viber-ui/src/graphql/queries.ts
+++ packages/plugin-viber-ui/src/graphql/queries.ts
@@ -1,65 +1,3 @@
-const messages: string = `
-query viberConversationMessages(
-  $conversationId: String!
-  $skip: Int
-  $limit: Int
-  $getFirst: Boolean
-) {
-  viberConversationMessages(
-    conversationId: $conversationId,
-    skip: $skip,
-    limit: $limit,
-    getFirst: $getFirst
-  ) {
-    _id
-    content
-    conversationId
-    customerId
-    userId
-    createdAt
-    isCustomerRead
-    internal
-
-    attachments {
-      url
-      name
-      type
-      size
-    }
-
-    user {
-      _id
-      username
-      details {
-        avatar
-        fullName
-        position
-      }
-    }
-
-    customer {
-      _id
-      avatar
-      firstName
-      middleName
-      lastName
-      primaryEmail
-      primaryPhone
-      state
-
-      companies {
-        _id
-        primaryName
-        website
-      }
-
-      customFieldsData
-      tagIds
-    }
-  }
-}
-`;
-
 const viberIntegrationDetail: string = `
   query viberIntegrationDetail($integrationId: String!) {
     viberIntegrationDetail(integrationId: $integrationId) {
@@ -70,6 +8,5 @@ const viberIntegrationDetail: string = `
 `;
 
 export default {
-  messages,
   viberIntegrationDetail
 };
