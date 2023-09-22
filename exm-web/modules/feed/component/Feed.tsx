7c50218872584ef9455e3335773686b5ccdecb9d
Enkhtuvshin Narmandakh <enkhtuvshin@Enkhtuvshins-MacBook-Pro.local>
2023-09-22 13:37:52 +0800

    update

--- exm-web/modules/feed/component/Feed.tsx
+++ exm-web/modules/feed/component/Feed.tsx
@@ -1,3 +1,9 @@
+"use client"
+
+import { usePosts } from "../hooks/usePosts"
+
 export const Feed = () => {
+  const { posts, postsCount, loading, handleLoadMore } = usePosts()
+
   return <div>123</div>
 }
